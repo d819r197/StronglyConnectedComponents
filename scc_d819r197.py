5629162170d9552ecb73abfd8a2a39cf3242b83e
Blake Rieschick <blake@awnix.com>
2020-03-05 17:07:07 -0600

    implemented grev

--- scc_d819r197.py
+++ scc_d819r197.py
@@ -1,6 +1,6 @@
 import sys
 
-verbose = True
+verbose = False
 
 class Node():
     def __init__(self, n):
@@ -41,7 +41,7 @@ class Graph():
                 self.discovered[d] = False
 
         #Initialize Layer 2D List
-        if verbose: print("Initilizing 2D List L, with first row: [" + str(initNode.id) + "]")
+        if verbose: print("Initilizing 2D List L, with first row: [" + str(initNode.id+1) + "]")
         L = []
         L.append([initNode])
 
@@ -53,30 +53,38 @@ class Graph():
 
         #Run while L[i] is not empty
         while len(L[i]) != 0:
-            print("Current i: " + str(i))
+            if verbose: print("Current i: " + str(i))
             #Initialize New List L[i+1]
             L.append([])
-            print("New size of L: " + str(len(L)))
+            if verbose: print("New size of L: " + str(len(L)))
             #For each node U in L[i]
             for u in L[i]:
-                if verbose: print("Current U: " + str(u.id))
+                if verbose: print("Current U: " + str(u.id+1))
                 #Consider Each Edge (u,v)
                 for v in u.connectedNodes:
-                    if verbose: print("Current V: " + str(v.id))
+                    if verbose: print("Current V: " + str(v.id+1))
+
                     if self.discovered[v.id] == False:
                         self.discovered[v.id] = True
 
                         #Add (u,v) to the tree
-                        if verbose: print("Adding (" + str(u.id) +","+str(v.id)+") to the tree")
-                        T.append((u.id,v.id))
+                        if verbose: print("Adding (" + str(u.id+1) +","+str(v.id+1)+") to the tree")
+                        T.append((u.id+1,v.id+1))
 
                         #Add v to the list L[i+1]
-                        if verbose: print("Adding V: "+str(v.id)+" to the list L")
+                        if verbose: print("Adding V: "+str(v.id+1)+" to the list L")
                         L[i+1].append(v)
             #Increment Layer Counter by 1
             i += 1
-        if verbose: print("L[i] is empty")
-        if verbose: print(T)
+        if verbose:
+            print("L[i] is empty")
+            print("T: " + str(T))
+        if False in self.discovered:
+            if verbose: print("dfs at node: " + str(initNode.id+1) + " has failed")
+            return False
+        else:
+            if verbose: print("dfs at node: " + str(initNode.id+1) + " has passed")
+            return True
 
     def deriveConnectedComponents(self):
         global verbose
@@ -102,12 +110,34 @@ def importNodes(filePath):
 
     return graph
 
+def reverse(oldGraph):
+    newGraph = Graph(oldGraph.nodeCount)
+    for n in oldGraph.nodeList:
+        for nc in n.connectedNodes:
+            newGraph.nodeList[nc.id].connectedNodes.append(newGraph.nodeList[n.id])
+    return newGraph
+
 def main():
     global verbose
     if verbose: print("Starting Program")
     g = importNodes(sys.argv[1])
-    g.bfs(g.nodeList[2])
-    g.printGraph()
+    grev = reverse(g)
+    scc = []
+
+    for n in range(len(g.nodeList)):
+        bfs1 = g.bfs(g.nodeList[n])
+        bfs2 = grev.bfs(g.nodeList[n])
+        if bfs1 and bfs2:
+            scc.append(n+1)
+
+    if verbose:
+        print("----------START G PRINT----------")
+        g.printGraph()
+        print("-----------END G PRINT-----------")
+        print("---------START GREV PRINT---------")
+        grev.printGraph()
+        print("----------END GREV PRINT----------")
+    print("SCC Output: " + str(scc))
 
 if __name__ == "__main__":
     main()
